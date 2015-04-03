6009cde5a3b7b8ae1339dd279671199906fa6ae2
qstar <davesla2012@hotmail.com>
2015-04-03 16:09:16 +0800

    angularjs http test

--- Foodie-web/src/main/webapp/angularjs/testController.js
+++ Foodie-web/src/main/webapp/angularjs/testController.js
@@ -1,8 +1,30 @@
 /**
  * 
  */
-app.controller("testController",function($scope){
-	$scope.firstname="John";
-	$scope.lastname="Doe";
+app.controller("testController", function($scope) {
+	$scope.firstName = "John";
+	$scope.lastName = "Doe";
 });
 
+app.controller("httpController", function($scope, $http) {
+	var dish = {
+		name : 1
+	};
+	$http({
+		method : 'GET',
+		url : "ws/dish/search",
+		params : dish
+	}).success(function(data) {
+		console.log(data);
+		$scope.res1 = data;
+		if (!data.errorCode) {
+			var dish=data.dish;
+			if(dish.length>0){
+				for(i=0;i<dish.length;i++){
+					$scope.res2=dish[i].name;
+				}
+			}
+			$scope.res3 = data;
+		}
+	})
+});
